602b189f3e76ec9193f9d8e2826336156ba09e98
XiangYang95 <xiangyang.5083@gmail.com>
2018-03-01 00:35:55 -0800

    Initial commit

---
+++ Assets/Script/Ball.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+	public Paddle paddle;
+	private Vector3 paddleToBallVector;
+	private bool hasStarted = false;
+	// Use this for initialization
+	void Start () {
+		paddleToBallVector = this.transform.position - paddle.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!hasStarted) {
+			//lock the ball relative to the paddle
+			this.transform.position = paddle.transform.position + paddleToBallVector;
+
+			//wait for a mouse press to lauch
+			if (Input.GetMouseButtonDown (0)) {
+				hasStarted = true;
+				this.GetComponent<Rigidbody2D> ().velocity = new Vector2 (2f, 2f);
+			}
+		}
+	}
+}
---
+++ Assets/Script/LoseCollider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseCollider : MonoBehaviour {
+	public LevelManager levelmanager;
+	void OnCollisionEnter2D(Collision2D collider){
+		levelmanager.LoadLevel ("Win");
+	}
+
+//	void OnTriggerEnter2D(Collider2D collider){
+//		print ("Trigger");
+//	}
+}
